142ed5ceb2cc896c1c23a373479914e378deaba3
yagnik-varu <yagnikvaru2003@gmail.com>
2023-08-28 10:49:32 +0530

    Set secret key path

--- backend_cc/api/views.py
+++ backend_cc/api/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 from firebase_admin import credentials, initialize_app, firestore
+import os
 
 
-cred = credentials.Certificate(r"D:\CampusConnect Group project\CampusConnect.github.io\backend_cc\api\SecretKey.json")
+SecretKey_path=os.getcwd()+"\\api\SecretKey.json"
+
+
+cred = credentials.Certificate(SecretKey_path)
 initialize_app(cred)
 db = firestore.client()
 root_ref = db.collection("Users")
