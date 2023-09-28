ea0e2681020c01acd5b6a22254669a04d396c07a
yash <yashpadaria22@gmail.com>
2023-09-28 15:50:08 +0530

    editlinkadd

--- campus-connect-app/src/components/user/UserProfileCard.js
+++ campus-connect-app/src/components/user/UserProfileCard.js
@@ -7,6 +7,7 @@ import img1 from '../../img1.webp'
 import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EditIcon from '@mui/icons-material/Edit';
 function UserProfileCard() {
   let userProfile = 
     {
@@ -22,69 +23,92 @@ function UserProfileCard() {
   
   return (
     <>
-      <br />  
-      <div className="container mt-5 " data-bs-theme="light">
-        <div className="row justify-content-center">
-          <div className="col-md-8">
-            <div className="card shadow">
-              <div className="card-body">
-                <div className="row ">
-                  <div className="col-md-4 d-flex flex-column align-items-center  ">
-                    <img
-                      src={userProfile.photo}
-                      alt="User Profile"
-                      className="img-fluid rounded-circle"
-                    />
-                    <br />
-                    <h2 className="mt-2"><strong>{userProfile.name}</strong></h2>
-                    <div className='d-flex gap-6'>
-                      {userProfile.insta ? <div >
-                        
-                        <a href={`//${userProfile.insta}`} target="_blank" rel="noopener noreferrer" className='text-decoration-none text-danger' ><InstagramIcon /></a>
-                        
-                      </div>:""
-                      }
-                      {userProfile.linkedin ? <div >
-                        
-                        <a href={`//${userProfile.linkedin}`} target="_blank" rel="noopener noreferrer" className='text-decoration-none text-primary'><LinkedInIcon /></a>
-                        
-                      </div>:""}
-                      {userProfile.twitter? <div >
-                        
-                        <a href={`//${userProfile.twitter}`} target="_blank" rel="noopener noreferrer" className='text-decoration-none text-primary'><TwitterIcon /></a>
-                        
-                      </div>:""}
-
-                    </div>
+<div className="container mt-5 " data-bs-theme="light">
+  <div className="row justify-content-center">
+    <div className="col-md-8">
+      <div className="card shadow">
+        <div className="card-body">
+          <div className="row">
+            <div className="col-md-4 d-flex flex-column align-items-center">
+              
+              <img
+                src={userProfile.photo}
+                alt="User Profile"
+                className="img-fluid rounded-circle"
+              />
+              <br />
+              <h2 className="mt-2"><strong>{userProfile.name}</strong></h2>
+              <div className='d-flex gap-6'>
+                {userProfile.insta ? (
+                  <div>
+                    <a
+                      href={`//${userProfile.insta}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className='text-decoration-none text-danger'
+                    >
+                      <InstagramIcon />
+                    </a>
                   </div>
-                  <div className="col-md-8 ">
-                    <p><EmailIcon /> <strong>Email: </strong> {userProfile.email}</p>
-                    <div className="mb-3">
-                      <AutoFixHighIcon /><strong>Interests:</strong>
-                      <ul>
-                        {userProfile.interest.map((interest, index) => (
-                          <li key={index}><span className="badge bg-black"> {interest}</span></li>
-                        ))}
-                        {/* {userProfile.map((user,index) => (
-                          <li key={index}><span className="badge bg-gray"> {user.interest}</span></li>
-                        ))} */}
-                      </ul>
-                    </div>
-                    <div>
-                      <strong  >Main Branch:</strong> {userProfile.main_branch}
-                    </div>
+                ) : ""}
+                {userProfile.linkedin ? (
+                  <div>
+                    <a
+                      href={`//${userProfile.linkedin}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className='text-decoration-none text-primary'
+                    >
+                      <LinkedInIcon />
+                    </a>
                   </div>
-                </div>
-                <div className="text-center mt-3">
-                  <button className="btn btn-danger " >
-                    Logout
-                  </button>
-                </div>
+                ) : ""}
+                {userProfile.twitter ? (
+                  <div>
+                    <a
+                      href={`//${userProfile.twitter}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className='text-decoration-none text-primary'
+                    >
+                      <TwitterIcon />
+                    </a>
+                  </div>
+                ) : ""}
+              </div>
+            </div>
+            <div className="col-md-8">
+            <div className="d-flex justify-content-end w-100">
+                {/* Add the "Edit Profile" link here */}
+                <a href="/edit-profile" className="btn btn-primary">
+                  <EditIcon />
+                </a>
+              </div>
+              <p><EmailIcon /> <strong>Email: </strong> {userProfile.email}</p>
+              <div className="mb-3">
+                <AutoFixHighIcon /><strong>Interests:</strong>
+                <ul>
+                  {userProfile.interest.map((interest, index) => (
+                    <li key={index}><span className="badge bg-black"> {interest}</span></li>
+                  ))}
+                </ul>
+              </div>
+              <div>
+                <strong>Main Branch:</strong> {userProfile.main_branch}
               </div>
             </div>
           </div>
+          <div className="text-center mt-3">
+            <button className="btn btn-danger">
+              Logout
+            </button>
+          </div>
         </div>
       </div>
+    </div>
+  </div>
+</div>
+
     </>
   )
 }
