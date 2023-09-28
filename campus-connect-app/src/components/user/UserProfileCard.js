28646ee36c589af1cdeb49c19bd40d8796993747
yagnik-varu <yagnikvaru2003@gmail.com>
2023-09-29 04:50:13 +0530

    login page data is arrived

--- campus-connect-app/src/components/user/UserProfileCard.js
+++ campus-connect-app/src/components/user/UserProfileCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {useEffect, useState} from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -14,9 +15,11 @@ import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import EditIcon from "@mui/icons-material/Edit";
-function UserProfileCard() {
+
+function UserProfileCard(props) {
+
   let userProfile = {
-    name: "xyz",
+    name: props.name,
     email: "xyz@gmail.com",
     photo: img1, // in navbar too, display in circle format.
     interest: ["abc", "pqr", "hao"],
@@ -25,108 +28,135 @@ function UserProfileCard() {
     linkedin: "www.linkedin.com",
     twitter: "www.twitter.com",
   };
+  console.log("in profile")  
+  const [user, setUser] = useState(null)
+    
+
+  useEffect(() =>{
+    // if local storage has data it remain as it is
+      setUser(JSON.parse(localStorage.getItem("user_config")))// so we set user info as 
+    
+  }, [props])  
+  const handleLogout=()=>{//when we click log out userdata remove 
+    // localStorage.clear()
+    setUser(null)
+    localStorage.setItem("user_config", null)// also delete from localstorage to
+    window.location.reload()
+  }
+  
+
 
   return (
-    <>
-      <div className="container mt-5 " data-bs-theme="light">
-        <div className="row justify-content-center">
-          <div className="col-md-8">
-            <div className="card shadow">
-              <div className="card-body">
-                <div className="row">
-                  <div className="col-md-4 d-flex flex-column align-items-center">
-                    <img
-                      src={userProfile.photo}
-                      alt="User Profile"
-                      className="img-fluid rounded-circle"
-                      style={{ height: "80%", width: "80%" }}
-                    />
 
-                    <br />
-                    <h2 className="mt-2">
-                      <strong>{userProfile.name}</strong>
-                    </h2>
-                    <div className="d-flex gap-6">
-                      {userProfile.insta ? (
-                        <div>
-                          <a
-                            href={`//${userProfile.insta}`}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="text-decoration-none text-danger"
-                          >
-                            <InstagramIcon />
-                          </a>
-                        </div>
-                      ) : (
-                        ""
-                      )}
-                      {userProfile.linkedin ? (
-                        <div>
-                          <a
-                            href={`//${userProfile.linkedin}`}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="text-decoration-none text-primary"
-                          >
-                            <LinkedInIcon />
-                          </a>
-                        </div>
-                      ) : (
-                        ""
-                      )}
-                      {userProfile.twitter ? (
-                        <div>
-                          <a
-                            href={`//${userProfile.twitter}`}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="text-decoration-none text-primary"
-                          >
-                            <TwitterIcon />
-                          </a>
-                        </div>
-                      ) : (
-                        ""
-                      )}
-                    </div>
-                  </div>
-                  <div className="col-md-8">
-                    <div className="d-flex justify-content-end w-100">
-                      {/* Add the "Edit Profile" link here */}
-                      <a href="/edit-profile" className="btn btn-primary">
-                        <EditIcon />
-                      </a>
-                    </div>
-                    <p>
-                      <EmailIcon /> <strong>Email: </strong> {userProfile.email}
-                    </p>
-                    <div className="mb-3">
-                      <AutoFixHighIcon />
-                      <strong>Interests:</strong>
-                      <ul>
-                        {userProfile.interest.map((interest, index) => (
-                          <li key={index}>
-                            <span className="badge bg-black"> {interest}</span>
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                    <div>
-                      <strong>Main Branch:</strong> {userProfile.main_branch}
-                    </div>
+    
+    <>
+    {
+      (user != null) ? <div className="container mt-5 " data-bs-theme="light">
+      <div className="row justify-content-center">
+        <div className="col-md-8">
+          <div className="card shadow">
+            <div className="card-body">
+              <div className="row">
+                <div className="col-md-4 d-flex flex-column align-items-center">
+                  <img
+                    src={user.photo}
+                    alt="User Profile"
+                    className="img-fluid rounded-circle"
+                    style={{ height: "80%", width: "80%" }}
+                  />
+
+                  <br />
+                  <h2 className="mt-2">
+                    <strong>{user.name}</strong>
+                  </h2>
+                  <div className="d-flex gap-6">
+                    {userProfile.insta ? (
+                      <div>
+                        <a
+                          href={`//${userProfile.insta}`}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="text-decoration-none text-danger"
+                        >
+                          <InstagramIcon />
+                        </a>
+                      </div>
+                    ) : (
+                      ""
+                    )}
+                    {userProfile.linkedin ? (
+                      <div>
+                        <a
+                          href={`//${userProfile.linkedin}`}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="text-decoration-none text-primary"
+                        >
+                          <LinkedInIcon />
+                        </a>
+                      </div>
+                    ) : (
+                      ""
+                    )}
+                    {userProfile.twitter ? (
+                      <div>
+                        <a
+                          href={`//${userProfile.twitter}`}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="text-decoration-none text-primary"
+                        >
+                          <TwitterIcon />
+                        </a>
+                      </div>
+                    ) : (
+                      ""
+                    )}
                   </div>
                 </div>
-                <div className="text-center mt-3">
-                  <button className="btn btn-danger">Logout</button>
+                <div className="col-md-8">
+                  <div className="d-flex justify-content-end w-100">
+                    {/* Add the "Edit Profile" link here */}
+                    <a href="/edit-profile" className="btn btn-primary">
+                      <EditIcon />
+                    </a>
+                  </div>
+                  <p>
+                    <EmailIcon /> <strong>Email: </strong> {user.email}
+                  </p>
+                  <div className="mb-3">
+                    <AutoFixHighIcon />
+                    <strong>Interests:</strong>
+                    <ul>
+                      {userProfile.interest.map((interest, index) => (
+                        <li key={index}>
+                          <span className="badge bg-black"> {interest}</span>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                  <div>
+                    <strong>Main Branch:</strong> {userProfile.main_branch}
+                  </div>
                 </div>
+              </div>
+              <div className="text-center mt-3">
+                <button className="btn btn-danger" onClick={handleLogout}>Logout</button>
               </div>
             </div>
           </div>
         </div>
       </div>
+    </div>:<h2></h2>
+}
+    
+      
     </>
   );
 }
 
 export default UserProfileCard;
+
+
+
+
--- campus-connect-app/src/pages/UserProfile.js
+++ campus-connect-app/src/pages/UserProfile.js
@@ -1,17 +1,64 @@
-import React from 'react'
-import Auth from '../components/Auth'
+// import React,{useState} from 'react'
+// import Auth from '../components/Auth'
 import UserProfileCard from '../components/user/UserProfileCard'
 
-function UserProfile() {
-  return (
-    <>
+import {auth, provider} from '../components/FirebaseConfig'; // fire base initailaization
+import {useEffect, useState} from 'react';
+import { signInWithPopup } from 'firebase/auth'; // google interface sign in with 
+import axios from 'axios' // npm install axios
 
-   
-        <Auth></Auth>
-        <UserProfileCard />
-    </>
-   
-  )
+function UserProfile(){
+    const [user, setUser] = useState(null)
+    
+
+    useEffect(() =>{
+      if(localStorage.getItem("user_config")){ // if local storage has data it remain as it is
+        setUser(JSON.parse(localStorage.getItem("user_config")))// so we set user info as 
+      }
+    }, [])// here [] present empty dependancy on page load 
+
+    const handleGoogleSignIn=()=>{
+        signInWithPopup(auth, provider).then((result)=>{
+          console.log("user data",result)
+
+          localStorage.setItem("user_config", JSON.stringify({ // store sign in data in local storage insted of fetch from google 
+            email: result.user.email,
+            name: result.user.displayName,
+            photo: result.user.photoURL
+          }))
+          
+          setUser(JSON.parse(localStorage.getItem("user_config")))// to use details in react components
+          
+          //sendin data to django to store in firestore
+          axios.get("http://127.0.0.1:8000/auth/", {
+            params:{
+              email: result.user.email,
+              name: result.user.displayName,
+              photo: result.user.photoURL
+            }})
+            .then(response => console.log(response.data))
+            .catch(error => console.log(error))
+
+        }).catch((err)=>{
+          // console.log(err);
+        })
+      }
+
+      const handleLogout=()=>{//when we click log out userdata remove 
+        // localStorage.clear()
+        setUser(null)
+        localStorage.setItem("user_config", null)// also delete from localstorage to
+      }
+    
+    return (
+        <>
+           
+            {user ? <UserProfileCard name={user.name} email={user.email} photo={user.photo} ></UserProfileCard>:
+            <button onClick={handleGoogleSignIn}>
+              <h1> click here to login your account</h1></button>}
+            
+        </>
+    )
 }
 
 export default UserProfile
